9332764b0685ed56b6dacb977b154aae79592e87
Rahul <rtanwar@users.noreply.github.com>
2015-06-10 12:51:04 +0530

    add basic authentication

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ var http    = require("http");              // http server core module
 var express = require("express");           // web framework external module
 var io      = require("socket.io");         // web socket external module
 var easyrtc = require("easyrtc");           // EasyRTC external module
-
+//var fs = require('fs');
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var httpApp = express();
+httpApp.use(express.basicAuth('User', 'EMPLOYEESPROVIDENTFUNDORGANISATION'));
 httpApp.use(express.static(__dirname + "/static/"));
-
+//#httpApp.use(express.logger('dev')); //short tiny
+httpApp.use(express.logger());
+//var logFile = fs.createWriteStream('./myLogFile.log', {flags: 'a'}); //use {flags: 'w'} to open in write mode
+//httpApp.use(express.logger({stream: logFile}));
 // Start Express http server on port 8080
 var webServer = http.createServer(httpApp).listen(8080);
 
